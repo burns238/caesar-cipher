bc4c51ebf7de6b78efa82dfa6a8faff36ad3d468
Mike Burns <burns.23@hotmail.co.uk>
2019-05-20 14:27:39 +0100

    Fixed limitations in length
    - Outstanding issue whereby long texts take a long time

--- src/main/scala/dataencryptionstandard/DES.scala
+++ src/main/scala/dataencryptionstandard/DES.scala
@@ -8,6 +8,12 @@ import dataencryptionstandard.Round._
 object DES {
 	
 	private def crypto(plainText: String, subKeys: Seq[Vector[Char]]): String = {
+		val textChunks: Vector[String] = plainText.grouped(8).toVector
+
+		textChunks.map(c => crypt8Characters(c.padTo(8,' '), subKeys)).mkString
+	}
+
+	private def crypt8Characters(plainText: String, subKeys: Seq[Vector[Char]]): String = {
 		val (leftBits, rightBits) = (initialPermutation andThen split64)(stringToBits(plainText))
 		val postRoundsBits = round(leftBits, rightBits, subKeys, 1)
 		val finalBits = (swap _ andThen inversePermutation)(postRoundsBits)
@@ -22,7 +28,7 @@ object DES {
 	//To decrypt, we use the same algorithm but reverse the subkeys we generate from the key passed in
 	def decrypt(plainText: String, key: String): String = {
 		val subKeys = generateSubKeys(key).reverse
-		crypto(plainText, subKeys)
+		crypto(plainText, subKeys).trim
 	}
 
 }
--- src/test/scala/dataencryptionstandard/CryptoSpec.scala
+++ src/test/scala/dataencryptionstandard/CryptoSpec.scala
@@ -33,4 +33,35 @@ class CryptoSpec extends FlatSpec with Matchers {
     DES.decrypt(encrypted, key) shouldEqual initialString
   }
 
+  it should "decrypt a string longer than 8 characters" in {
+  	val initialString = "LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT ITA CUM EA VOLUNT RETINERE QUAE SUPERIORI SENTENTIAE CONVENIUNT IN ARISTONEM INCIDUNT"
+  	val key = "aa236eGh"
+    val encrypted = DES.encrypt(initialString, key)
+    DES.decrypt(encrypted, key) shouldEqual initialString
+  }
+
+  it should "decrypt a string shorter than 8 characters" in {
+  	val initialString = "Ab3"
+  	val key = "aa236eGh"
+    val encrypted = DES.encrypt(initialString, key)
+    DES.decrypt(encrypted, key) shouldEqual initialString
+  }
+
+  it should "decrypt a string that's ridiculously long" in {
+  	val initialString = LoremIpsumHelper.LoremIpsum
+  	val key = "aa236eGh"
+    val encrypted = DES.encrypt(initialString, key)
+    DES.decrypt(encrypted, key) shouldEqual initialString
+  }
+
+  it should "decrypt a string that's ridiculously long in a short amount of time" in {
+  	val initialString = LoremIpsumHelper.LoremIpsum
+  	val key = "aa236eGh"
+  	val startingTime = System.currentTimeMillis();
+    val encrypted = DES.encrypt(initialString, key)
+    val decrypted = DES.decrypt(encrypted, key) 
+    val endingTime = System.currentTimeMillis();
+    (endingTime - startingTime).toInt should be < 10
+  }
+
 }
